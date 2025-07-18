f3d697bd7c1e4520b647638ec9cd453004dc8501
miguelmachadofs <miguelmfs121@gmail.com>
2025-07-18 12:15:18 -0300

    refactor: modularizacao do recebimento e validacao da placa do veiculo do usuario

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -12,65 +12,57 @@ class Program
         ExibirMenu();
 
         string opcao = Console.ReadLine() ?? "";
-        string placa;
+        string placa = string.Empty;
 
         do
         {
             switch (opcao)
             {
                 case "1":
-                    // Valida a placa do veículo informada pelo usuário
-                    Console.WriteLine("Digite a placa do veículo:");
-                    placa = Console.ReadLine() ?? "";
-                    if (string.IsNullOrWhiteSpace(placa))
+                    // Obtém e Valida a placa do veículo a ser cadastrado
+                    if (PlacaVeiculoEhValida(PerguntaPlacaVeiculo(ref placa)))
                     {
-                        Console.WriteLine("Placa inválida. Tente novamente.");
-                        continue;
-                    }
-                    // Verifica se o veículo já está cadastrado
-                    // Se não estiver, cadastra o veículo no estacionamento
-                    if (!VeiculoEstaCadastrado(placa, estacionamento.Veiculos))
-                    {
-                        estacionamento.CadastrarVeiculo(placa);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Veículo já cadastrado.");
-                    }
-                    break;
-                case "2":
-                    // Valida a placa do veículo informada pelo usuário
-                    Console.WriteLine("Digite a placa do veículo:");
-                    placa = Console.ReadLine() ?? "";
-                    if (string.IsNullOrWhiteSpace(placa))
-                    {
-                        Console.WriteLine("Placa inválida. Tente novamente.");
-                        continue;
-                    }
-                    // Verifica se o veículo está cadastrado
-                    // Se estiver, calcula o valor total a pagar e remove o veículo do estacionamento
-                    if (VeiculoEstaCadastrado(placa, estacionamento.Veiculos))
-                    {
-                        var veiculo = estacionamento.Veiculos.FirstOrDefault(v => v.Placa == placa);
-                        if (veiculo != null)
+                        // Verifica se o veículo já está cadastrado
+                        // Se não estiver, cadastra o veículo no estacionamento
+                        if (!VeiculoEstaCadastrado(placa, estacionamento.Veiculos))
                         {
-                            // Calcular o valor total a pagar
-                            decimal valorTotal = estacionamento.CalcularValorTotal(veiculo.DataEntrada);
-                            Console.WriteLine($"Valor total a pagar: R$ {valorTotal:F2}");
-                            Console.ReadLine(); // Espera o usuário pressionar Enter para continuar
-                            Console.WriteLine($"Veículo com placa {placa} disponível para retirada.");
-
-                            // Remover o veículo do estacionamento
-                            estacionamento.RemoverVeiculo(veiculo);
+                            estacionamento.CadastrarVeiculo(placa);
                         }
                         else
                         {
-                            Console.WriteLine("ERRO: Veículo não encontrado.");
+                            Console.WriteLine("Veículo já cadastrado.");
                         }
                     }
-                    else
+                    break;
+                case "2":
+                    // Obtém e Valida a placa do veículo a ser retirado
+                    if (PlacaVeiculoEhValida(PerguntaPlacaVeiculo(ref placa)))
                     {
-                        Console.WriteLine("Veículo não encontrado. Verifique a placa e tente novamente.");
+                        // Verifica se o veículo está cadastrado
+                        // Se estiver, calcula o valor total a pagar e remove o veículo do estacionamento
+                        if (VeiculoEstaCadastrado(placa, estacionamento.Veiculos))
+                        {
+                            var veiculo = estacionamento.Veiculos.FirstOrDefault(v => v.Placa == placa);
+                            if (veiculo != null)
+                            {
+                                // Calcular o valor total a pagar
+                                decimal valorTotal = estacionamento.CalcularValorTotal(veiculo.DataEntrada);
+                                Console.WriteLine($"Valor total a pagar: R$ {valorTotal:F2}");
+                                Console.ReadLine(); // Espera o usuário pressionar Enter para continuar
+                                Console.WriteLine($"Veículo com placa {placa} disponível para retirada.");
+
+                                // Remover o veículo do estacionamento
+                                estacionamento.RemoverVeiculo(veiculo);
+                            }
+                            else
+                            {
+                                Console.WriteLine("ERRO: Veículo não encontrado.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Veículo não encontrado. Verifique a placa e tente novamente.");
+                        }
                     }
                     break;
                 case "3":
@@ -102,5 +94,23 @@ class Program
     private static bool VeiculoEstaCadastrado(string placa, List<VeiculoEstacionamento> veiculos)
     {
         return veiculos.Any(v => v.Placa.Equals(placa, StringComparison.OrdinalIgnoreCase));
+    }
+
+    private static string PerguntaPlacaVeiculo(ref string placa)
+    {
+        // Obtém a placa do veículo informada pelo usuário
+        Console.WriteLine("Digite a placa do veículo:");
+        placa = Console.ReadLine()?.Trim().ToUpper() ?? string.Empty;
+        return placa;
+    }
+
+    private static bool PlacaVeiculoEhValida(string placa)
+    {
+        if (string.IsNullOrEmpty(placa))
+        {
+            Console.WriteLine("Placa inválida. A placa não pode ser vazia.");
+            return false;
+        }
+        return true;
     }
 }
